76466240a93890ce279357b62d3cbdc988606bd8
Atai <abektenaliev@inbox.ru>
2022-11-07 19:54:22 +0600

    new commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Atai");
         System.out.println("Bektenaliev");
+        System.out.println("Sakebaevich");
     }
 }
